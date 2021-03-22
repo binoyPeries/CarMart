a269100f7d379535934f2c64754ce2f4828a5454
binoy <binoy.18@cse.mrt.ac.lk>
2021-03-22 23:40:30 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './index.css';
 import NaviBar from './components/navbar';
+import Home from './components/home';
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
+
 
 function App() {
   return (
-    <NaviBar/>
+    <Router>
+    <div className="div">
+      <NaviBar/>
+      <div className="">
+      <Switch>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route exact path='/more'>
+          <h1>hello</h1>
+        </Route>
+      </Switch>
+      </div>
+    </div>
+
+    </Router>
+    
+    
   );
 }
 
