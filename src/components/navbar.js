527b3e222c76ccbae7f818a12adebe90a19ca257
binoy <binoy.18@cse.mrt.ac.lk>
2021-03-22 09:28:50 +0530

    nav bar modified

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,18 @@
 import Navbar  from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav' ;
+import logo from '../images/logo1-sm.png'
 
 const NaviBar = () => {
     return ( 
-        <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar className='navbar-custom' expand="lg">
+        <Navbar.Brand href="/">
+    
+      <img
+        src={logo}
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
