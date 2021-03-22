6dd6c56a8510ecb5478dd4931181a7ac64c0a19e
binoy <binoy.18@cse.mrt.ac.lk>
2021-03-22 18:11:57 +0530

    navbar done

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,9 @@
 import Navbar  from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav' ;
-import logo from '../images/logo1-sm.png'
+import logo from '../images/logo1-sm2.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCar,faUserCircle, car } from '@fortawesome/free-solid-svg-icons'
+
 
 const NaviBar = () => {
     return ( 
@@ -10,14 +13,41 @@ const NaviBar = () => {
       <img
         src={logo}
         className="d-inline-block align-top"
-        alt="React Bootstrap logo"
+        alt="company-logo"
       />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+        <Navbar.Collapse id="basic-navbar-nav justify-content-end">
+            
+            <Nav.Link href="/" className='nav-item'>
+            
+            <button className="nav-btn btn btn-outline-primary " >
+
+            Market
+            </button>
+            </Nav.Link>
+            <Nav className="ml-auto nav-custom" >
+
+            {/* no user logged in */}
+            {/* <Nav.Link href="/" className='nav-item'>
+              <button className="nav-btn btn btn-outline-primary ">Register</button>
+            </Nav.Link>
+            <Nav.Link href="/" className='nav-item'>
+            <button className="nav-btn btn btn-outline-primary " >Login</button>
+            </Nav.Link> */}
+
+            {/* user logged in */}
+            <Nav.Link href="/" className='nav-item'>
+            <FontAwesomeIcon icon={faCar}  size="2x" style={{color:'#25A7F7'}}/>
+            </Nav.Link>
+            <Nav.Link href="/" className='nav-item'>
+            <FontAwesomeIcon icon={faUserCircle}  size="2x" style={{color:'#25A7F7'}}/>
+            </Nav.Link>
+            <Nav.Link href="/" className='nav-item'>
+            <button className="nav-btn btn btn-outline-primary " >Logout</button>
+            </Nav.Link>
+            
+
             </Nav>
            
         </Navbar.Collapse>
