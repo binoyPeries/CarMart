e91494e562c55c81afa0c75f4400b80352095dcb
binoy <binoy.18@cse.mrt.ac.lk>
2021-03-24 08:21:44 +0530

    updated

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,6 +6,7 @@ import { faCar,faUserCircle, car } from '@fortawesome/free-solid-svg-icons'
 
 
 const NaviBar = () => {
+    
     return ( 
         <Navbar className='navbar-custom' expand="lg">
         <Navbar.Brand href="/">
@@ -29,23 +30,23 @@ const NaviBar = () => {
             <Nav className="ml-auto nav-custom" >
 
             {/* no user logged in */}
-            <Nav.Link href="/register" className='nav-item'>
+            {/* <Nav.Link href="/register" className='nav-item'>
               <button className="nav-btn btn btn-outline-primary ">Register</button>
             </Nav.Link>
             <Nav.Link href="/" className='nav-item'>
             <button className="nav-btn btn btn-outline-primary " >Login</button>
-            </Nav.Link>
+            </Nav.Link> */}
 
             {/* user logged in */}
-            {/* <Nav.Link  className='nav-item'>
+            <Nav.Link  href ='/'  className='nav-item'>
             <FontAwesomeIcon icon={faCar}  size="2x" style={{color:'#25A7F7'}}/>
             </Nav.Link>
-            <Nav.Link href="/" className='nav-item'>
+            <Nav.Link href="/profile" className='nav-item'>
             <FontAwesomeIcon icon={faUserCircle}  size="2x" style={{color:'#25A7F7'}}/>
             </Nav.Link>
             <Nav.Link href="/" className='nav-item'>
             <button className="nav-btn btn btn-outline-primary " >Logout</button>
-            </Nav.Link> */}
+            </Nav.Link>
             
 
             </Nav>
