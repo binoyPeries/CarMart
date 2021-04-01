5db6ce44aac9e586eca45a8050fa6a0b7f67d303
binoy <binoy.18@cse.mrt.ac.lk>
2021-04-01 13:41:17 +0530

    vehicle types added

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,14 @@ export const Colors ={
     primaryColor: '#25A7F7', //logo light blue color 
 
 }
+
+export const VehicleTypes = [
+    'Car',
+    'SUV/Jeep',
+    'Van',
+    'Motorbike',
+    'Three Wheeler',
+    'Bus',
+    'Heavy Vehicle'
+
+]
