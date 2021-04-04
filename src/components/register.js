9a4280072868d25a6858ffcce64b4c6a79ecec32
binoy <binoy.18@cse.mrt.ac.lk>
2021-04-04 23:22:12 +0530

    register page updated

--- src/components/register.js
+++ src/components/register.js
@@ -4,6 +4,8 @@ import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Cities } from '../constants';
+
 
 
 
@@ -17,7 +19,7 @@ const Register = () => {
             <Card.Body>
             <Form>
                 <Row>
-                    <Col>
+                    <Col lg={6} >
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label className="form-text">First Name*</Form.Label>
                             <Form.Control type="text" placeholder=""  />
@@ -25,7 +27,7 @@ const Register = () => {
 
                     </Col>
                 
-                    <Col>
+                    <Col lg={6} >
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label className="form-text">Last Name*</Form.Label>
                             <Form.Control type="text" placeholder=""  />
@@ -35,16 +37,45 @@ const Register = () => {
 
                 </Row>
                 <Row>
-                    <Col >
+                    <Col lg={6}>
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label className="form-text">Email*</Form.Label>
                             <Form.Control type="email" placeholder=""  />
                         </Form.Group>
 
                     </Col>
+                    <Col lg={6} >
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label className="form-text">Contact No*</Form.Label>
+                            <Form.Control type="number" placeholder=""  />
+                        </Form.Group>
+
+                    </Col>
                 
                 </Row>
+                <Row>
+                    <Col lg={6}>
+                    <Form.Group controlId="formGridState">
+                        <Form.Label className="form-text">City</Form.Label>
+                        <Form.Control as="select" defaultValue="Choose...">
+                            <option style={{color:"#1a6aae",fontWeight:"bold"}}>Select City</option>
+                            {Cities.map((type)=> {
+                                return(<option value={type} style={{color:"#1a6aae"}}> {type} </option>)
+                            }
+                            )}
+                    </Form.Control>
+                    </Form.Group>
+                    </Col>
+
+                    <Col lg={6} style={{paddingTop:"40px"}}>
+                        
+                        <div className="" style={{color:"#25A7F7", fontSize:"15px"}}>
+                        (city will be used as your location when posting your Ads )
+                        </div>  
+
+                    </Col>
                 
+                </Row>
                 <Row>
                     <Col>
                         <Form.Group controlId="formBasicPassword">
