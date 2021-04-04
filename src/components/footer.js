3e63d8266311681a77d3a1d63d7cfe28bfc4d55f
binoy <binoy.18@cse.mrt.ac.lk>
2021-04-04 23:59:17 +0530

    footer updated #9

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopyright } from '@fortawesome/free-solid-svg-icons';
-import { faInstagram, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faCopyright, faVoicemail ,faInbox} from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faFacebook, faTwitter ,  } from '@fortawesome/free-brands-svg-icons';
 import logo from '../images/logo1-sm2.png'
 
 
@@ -16,33 +16,35 @@ const Footer = () => {
         <div className="footer">
             <Container>
             <Row className="justify-content-md-start">
-                <Col xs lg="6">
-                <img src={logo} alt="company-logo" style={{width:"250px"}}/>
+                <Col  sm ={12} md={12} lg={6} style={{paddingRight:"80px", paddingBottom:"20px"}}>
+                <h5>About Us</h5>
+                <p style={{textAlign:"justify",paddingTop:"10px"}}> <b>CarMart</b> is a one stop e-commerce platform for your favourite vehicles.
+                You can Buy, Sell, Trade  any type of vehicle, across the country, anytime, anywhere and absolutely free of Charge.
+                Posting an advertisement on Careka.lk is so convenient and takes only few minutes to get your vehicle advertised for sale, online.</p>
                     
                 </Col>
-                <Col xs lg="3">
-                    <Row>
-                    <p>Contact Us</p>
-                    </Row>
-                    <Row>
-                    <p>+94 777 123 456</p>
-                    </Row>
-                    <Row>
-                    <p>+94 777 456 456</p>
-                    </Row>
-                    
-                
+                <Col  sm={6} md={6} lg={3} style={{paddingRight:"80px", paddingBottom:"20px"}}>
+                    <h5>Contact Us</h5>   
+                    <div style={{paddingBottom:"5px",paddingTop:"10px"}}>
+                    <FontAwesomeIcon icon={faVoicemail}  size="sm" />
+                    <span> +94 777 123 456</span>
+                    </div>
+                    <div style={{paddingBottom:"5px"}} >
+                    <FontAwesomeIcon icon={faVoicemail}  size="sm" />
+                    <span>  +94 777 256 456</span>
+                    </div>
+                    <div style={{paddingBottom:"5px"}}>
+                    <FontAwesomeIcon icon={faInbox}  size="sm" />
+                    <span>  carmart@gmail.com</span>
+                    </div>
+                                  
                 </Col>
 
-                <Col xs lg="3">
-                    <Row>
-                    <p>Social Media</p>
-                    </Row>
-                    <Row>
-                        <span className="social-icon"> <FontAwesomeIcon icon={faInstagram}  size="lg" /> </span>
-                        <span  className="social-icon"> <FontAwesomeIcon icon={faFacebook}  size="lg" /> </span>
-                        <span  className="social-icon"> <FontAwesomeIcon icon={faTwitter}  size="lg" /> </span>
-                    </Row>
+                <Col sm={6} md={6} lg={3} style={{ paddingBottom:"20px"}} >
+                    <h5 style={{paddingBottom:"10px"}}>Social Media</h5>
+                    <span className="social-icon"> <FontAwesomeIcon icon={faInstagram}  size="lg" /> </span>
+                    <span  className="social-icon"> <FontAwesomeIcon icon={faFacebook}  size="lg" /> </span>
+                    <span  className="social-icon"> <FontAwesomeIcon icon={faTwitter}  size="lg" /> </span>
 
                 </Col>
 
