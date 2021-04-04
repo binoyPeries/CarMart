f3c4bfa6c4907cc7a711c25221d7ccfbe9bc1554
binoy <binoy.18@cse.mrt.ac.lk>
2021-04-04 23:21:56 +0530

    cities added

--- src/constants.js
+++ src/constants.js
@@ -14,3 +14,31 @@ export const VehicleTypes = [
     'Heavy Vehicle'
 
 ]
+
+export const Cities = [
+    'Ampara',
+    'Anuradhapura',
+    'Badulla',
+    'Batticaloa',
+    'Colombo',
+    'Galle',
+    'Gampaha',
+    'Hambantota',
+    'Jaffna',
+    'Kalutara',
+    'Kandy',
+    'Kegalle',
+    'Kilinochchi',
+    'Kurunegala',
+    'Mannar',
+    'Matale',
+    'Matara',
+    'Monaragala',
+    'Mullaitivu',
+    'Nuwara Eliya',
+    'Polonnaruwa',
+    'Puttalam',
+    'Ratnapura',
+    'Trincomalee',
+    'Vavuniya'
+]
