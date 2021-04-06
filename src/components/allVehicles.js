61f2ef1618bab0fc580a4f1e57dfe916d772806b
binoy <binoy.18@cse.mrt.ac.lk>
2021-04-06 09:12:28 +0530

    market page created

---
+++ src/components/allVehicles.js
@@ -0,0 +1,15 @@
+import { Container } from "react-bootstrap";
+import Filter from "./filter";
+
+const Market = () => {
+    return ( 
+        <Container fluid>
+            <Container fluid style={{marginTop:"40px"}}>
+                <Filter/>
+            </Container>
+
+        </Container>
+     );
+}
+ 
+export default Market;
