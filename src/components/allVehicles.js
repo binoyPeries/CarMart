d31975326ac3464fa04af621d4f6fbd0fa9f3fd1
binoy <binoy.18@cse.mrt.ac.lk>
2021-04-12 07:39:57 +0530

    partially done no pagination

--- src/components/allVehicles.js
+++ src/components/allVehicles.js
@@ -1,11 +1,35 @@
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import Filter from "./filter";
+import VehicleCard from "./vehicleCard";
 
 const Market = () => {
     return ( 
         <Container fluid>
             <Container fluid style={{marginTop:"40px"}}>
                 <Filter/>
+            </Container>
+
+            <Container fluid style={{marginTop:"50px" }}>
+                <h3 style={{marginBottom:"0px", color:"#1a6aae"}}>FEATURED VEHICLES</h3>
+                <hr className="main-heading-hr" />
+
+                <Row className="justify-content-center" >
+                    <Col  xl={6} style={{marginBottom:"20px" }}  >
+                    <VehicleCard liked={false} isprofileCard={false} />
+
+                    </Col>
+
+                    <Col  xl={6} style={{marginBottom:"20px"}} >
+                    <VehicleCard liked={true} isprofileCard={false} />
+
+                    </Col>
+
+          
+                </Row>
+
+              
+
+
             </Container>
 
         </Container>
